3604b7aa8ac1b602830a621452d7707b86fb163c
Cedrick <cedrickvalencia811@gmail.com>
2025-01-29 01:19:30 +0800

    Setup React and add about page

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { BrowserRouter as  Router } from 'react-router-dom';
+import './App.css';
+import Education from './components/Education';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import { darkTheme } from "./utils/Themes";
+import styled, { ThemeProvider } from "styled-components";
+
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg };
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg, 
+      rgba(204, 0, 187, 0.15) 0%, 
+      rgba(201, 32, 184, 0) 50%
+    ), 
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30 98%, 0 100%);
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,153 @@
+export const Bio = {
+    name: "Cedrick B. Valencia",
+    roles: [
+        "Software Engineer",
+        "Backend Developer",
+        "Frontend Developer"
+    ],
+    description: "Experienced Software Engineer specializing in Ruby on Rails development. Skilled in building web applications, creating RESTful APIs, optimizing databases, and integrating third-party services. Passionate about delivering clean, maintainable, and user-friendly solutions while collaborating effectively with teams to achieve project goals. Strong engineering professional holding a Bachelor of Technology in Information Technology from the Technological University of the Philippines.",
+    github: "https://github.com/cdrk811",
+    resume: "https://drive.google.com/file/d/1iIz88-et2r0JuMpRcOL1wdZhkm0LdL6l/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/cedrick-v-b79253233/",
+    // twitter: "",
+    // insta: "",
+    // facebook: ""
+};
+
+export const skills = [
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "ReactJS",
+                image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+            },
+            {
+                name: "NextJS",
+                image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+            },
+            {
+                name: "HTML",
+                image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+            },
+            {
+                name: "CSS",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+            },
+            {
+                name: "JavaScript",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            },
+            {
+                name: "Bootstrap",
+                image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+            {
+                name: "TailwindCSS",
+                image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+            {
+                name: "JQuery",
+                image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+            {
+                name: "Figma",
+                image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            }
+            
+        ]
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "MySQL",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+            },
+            {
+                name: "PostgreSQL",
+                image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+            },
+            {
+                name: "Firebase",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+        ]
+    },
+    {
+        title: "Programming Languages",
+        skills: [
+            {
+                name: "PHP",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+            {
+                name: "Ruby",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+            {
+                name: "JavaScript",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            }
+        ]
+    },
+    {
+        title: "Frameworks",
+        skills: [
+            {
+                name: "Laravel",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+            {
+                name: "Ruby on Rails",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+        ]
+    },
+    {
+        title: "CMS",
+        skills: [
+            {
+                name: "WordPress",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+        ]
+    },
+    {
+        title: "Other Tools",
+        skills: [
+            {
+                name: "Git",
+                image: "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+            },
+            {
+                name: "Github",
+                image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            },
+            {
+                name: "Docker",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+            },
+            {
+                name: "VS Code",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            },
+            {
+                name: "Postman",
+                image: "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+            },
+            {
+                name: "Figma",
+                image: "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+            },
+            {
+                name: "Rubymine",
+                image: "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+            },
+            {
+                name: "Datagrip",
+                image: "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+            },
+        ]
+    }
+];
