d152d6049ddbbfb64db4645e57ea12ea5738de37
Cedrick <cedrickvalencia811@gmail.com>
2025-01-30 18:14:18 +0800

    add projects page and style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Education from './components/Education';
 import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
-import { darkTheme } from "./utils/Themes";
+import { darkTheme, lightTheme } from "./utils/Themes";
 import styled, { ThemeProvider } from "styled-components";
 import Experience from './components/Experience';
+import Project from './components/Projects';
+import { useState } from 'react';
+import ProjectDetails from './components/ProjectDetails';
 
 const Body = styled.div`
   background-color: ${({theme}) => theme.bg };
@@ -15,21 +18,14 @@ const Body = styled.div`
 `;
 
 const Wrapper = styled.div`
-  background: linear-gradient(
-      38.73deg, 
-      rgba(204, 0, 187, 0.15) 0%, 
-      rgba(201, 32, 184, 0) 50%
-    ), 
-    linear-gradient(
-      141.27deg,
-      rgba(0, 70, 209, 0) 50%,
-      rgba(0, 70, 209, 0.15) 100%
-    );
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 30 98%, 0 100%);
 `;
 
 function App() {
+  const [openModal, setOpenModal] = useState({ state: false, project: null })
+  console.log(openModal)
   return (
     <ThemeProvider theme={darkTheme}>
       <Router>
@@ -39,8 +35,13 @@ function App() {
           <Wrapper>
             <Skills />
             <Experience />
+          </Wrapper>
+          <Project openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
             <Education />
           </Wrapper>
+          {/* <Footer /> */}
+          {openModal.state && <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />}
         </Body>
       </Router>
     </ThemeProvider>
---
+++ src/components/Cards/ProjectCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, Image, Tags, Tag, Details, Title, Date, Description, Members, Avatar } from "./ProjectCardStyle";
+
+const ProjectCard = ({ project, setOpenModal }) => {
+    return (
+        <Card onClick={() => setOpenModal({state: true, project: project})}>
+            <Image src={project.image} />
+            <Tags>
+                {project.tags?.map((tag, index) => (
+                    <Tag>{tag}</Tag>
+                ))}
+            </Tags>
+            <Details>
+                <Title>{project.title}</Title>
+                <Date>{project.date}</Date>
+                <Description>{project.description}</Description>
+            </Details>
+            <Members>
+                {project.member?.map((member) => (
+                    <Avatar src={member.img} />
+                ))}
+            </Members>
+            {/* <Button>View Project</Button> */}
+        </Card>
+    )
+}
+
+export default ProjectCard;
