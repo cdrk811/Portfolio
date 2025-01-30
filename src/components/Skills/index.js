b3ca11fc845f004337ad81988eb9880e94484a57
Cedrick <cedrickvalencia811@gmail.com>
2025-01-30 22:54:51 +0800

    add react tilt in skills page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { skills } from "../../data/constants";
-import { Container, Wrapper, Title, Desc, SkillsContainer, Skill, SkillTitle, SkillList, SkillItem, SkillImage } from './SkillStyle'
+import { Container, Wrapper, Title, Desc, SkillsContainer, Skill, SkillTitle, SkillList, SkillItem, SkillImage } from './SkillStyle';
+import { Tilt } from "react-tilt";
 
 const Skills = () => {
     return (
         <Container id="skills">
             <Wrapper>
                 <Title>Skills</Title>
-                <Desc>Here are some of my skills on which I have been working on for the past 4 years.</Desc>
+                <Desc style={{ marginBottom: "40px" }}>Here are some of my skills on which I have been working on for the past 4 years.</Desc>
 
                 <SkillsContainer>
-                    {skills.map((skill) => (
-                        <Skill>
-                            <SkillTitle>{skill.title}</SkillTitle>
-                            <SkillList>
-                                {skill.skills.map((item) => (
-                                    <SkillItem>
-                                        <SkillImage src={item.image} />
-                                        {item.name}
-                                    </SkillItem>
-                                ))}
-                            </SkillList>
-                        </Skill>
+                    {skills.map((skill, index) => (
+                        <Tilt>
+                            <Skill key={`skill-${index}`}>
+                                <SkillTitle>{skill.title}</SkillTitle>
+                                <SkillList>
+                                    {skill.skills.map((item, index_x) => (
+                                        <SkillItem key={`skill-x-${index_x}`}>
+                                            <SkillImage src={item.image} />
+                                            {item.name}
+                                        </SkillItem>
+                                    ))}
+                                </SkillList>
+                            </Skill>
+                        </Tilt>
                     ))}
                 </SkillsContainer>
             </Wrapper>
